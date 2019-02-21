32119319109baea8f1919f630e28a0ebffc667fb
Sandunga <sandra.cuevastorres@gmail.com>
2019-02-20 20:55:56 -0600

    ya se ven imagenes. nombre y title

--- src/main.js
+++ src/main.js
@@ -13,27 +13,27 @@ function passPage2(){
 document.getElementById("CHAMPIONS").addEventListener("click", passPage2);
 
 function passPage3(){
-  document.getElementById("COMMUN").style.display="block";
-  document.getElementById("CHAMPS").style.display="none";
   document.getElementById("WELCOME").style.display="none";
+  document.getElementById("CHAMPS").style.display="none";
+  document.getElementById("COMMUN").style.display="block";
 }
 document.getElementById("LEAGUE").addEventListener("click", passPage3);
 
 let champId= `
-<div class="box">
-<img src= "{champ.img}" alt="{champ.name}">
-<div class="name">
-<h3>{champ.name}</h3>
+<li class="box">
+<img src="{champ.splash}" width="300" height="150>
+<div class="id">
+<h3>{champ.id}</h3>
 </div>
 <div class="title">
 <h3>{champ.title}</h3>
 </div>
-</div>`;
+</li>`;
 
 let listHtml = dataarray.map(lol => {
   return champId
-  .replace("{champ.img}", lol.img)
-  .replace("{champ.name}",lol.name)
+  .replace("{champ.splash}", lol.splash)
+  .replace("{champ.id}",lol.name)
   .replace("{champ.title}",lol.title);
 });
 
@@ -41,5 +41,33 @@ let htmlString = listHtml.join('');
 
 let content = document.getElementById('content');
 
-content.innerText = htmlString;
+content.innerHTML = htmlString;
 
+/*function searchChamp(){
+
+  filterData(LOL.data, document.getElementById("nameChampion").value);
+  document.getElementById("nameChampion").value="";
+}
+
+    let filterData=(data,condition) => {
+  for(let i=0; i<data.length; i++){
+    let champion = data[i];
+    if (lol.id == condition) {
+      printFirstData(champion);
+
+     }
+  }
+}
+
+
+
+ function printFirstData(champion){
+   let image=document.getElementById("info1");
+   let pokedex=document.getElementById("championBox");
+
+   pokedex.innerHTML= "Name: " +  champion.id + "<br>" + "<br>" + "Role: " + champion.tag + 
+                       "<br>" "Title:  " + champion.title +"<br>" + "Blurb:  " + champion.blurb +
+                       "<br>" "Info:  " + champion.info +"<br>" + "Stats:  " + champion.stats;
+                      
+
+   image.setAttribute("src", ""+champion.img);*/
